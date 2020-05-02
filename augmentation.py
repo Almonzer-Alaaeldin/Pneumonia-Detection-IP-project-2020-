355102e073877145dd6ceb784213ece692d977d6
Almonzer-Alaaeldin <bronzemonzer1@gmail.com>
2020-05-02 21:01:28 +0200

    added training and augmentation files

---
+++ augmentation.py
@@ -0,0 +1,51 @@
+######################################### create augmented dataset start #########################################
+
+
+from numpy import expand_dims
+from cv2 import resize
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+# from matplotlib import pyplot
+from os import listdir
+
+
+###################### Training Data Augmentation ######################
+
+newSize = 150
+
+for Normal_img in listdir('train/NORMAL/'):
+    if not Normal_img == '.DS_Store':
+        img = load_img('train/NORMAL/' + Normal_img)
+        # convert to numpy array
+        data = resize(img_to_array(img), (newSize, newSize))
+        # expand dimension to one sample
+        samples = expand_dims(data, 0)
+        # create image data augmentation generator
+        # datagen = ImageDataGenerator(rotation_range=45, horizontal_flip='True')
+        datagen = ImageDataGenerator(rotation_range=30, zoom_range=(.65, 1.))
+        # prepare iterator
+        it = datagen.flow(samples, batch_size=32, save_prefix='norm',save_to_dir='aug_testing/augments_dir/Normal')
+        
+        for i in range(600): next(it)
+
+ 
+
+for Pneum_img in listdir('train/PNEUMONIA/'):
+    if not Pneum_img == '.DS_Store':
+        img = load_img('train/PNEUMONIA/' + Pneum_img)
+        # convert to numpy array
+        data = resize(img_to_array(img), (newSize, newSize))
+        # expand dimension to one sample
+        samples = expand_dims(data, 0)
+        # create image data augmentation generator
+        # datagen = ImageDataGenerator(rotation_range=45, horizontal_flip='True')
+        datagen = ImageDataGenerator(rotation_range=30, zoom_range=(.65, 1.))
+        # prepare iterator
+        it = datagen.flow(samples, batch_size=32, save_prefix='pneum', save_to_dir='aug_testing/augments_dir/Pneumonia')
+        
+        for i in range(200): next(it)
+
+
+######################################### create augmented dataset end #########################################
+
---
+++ model.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+
+from sklearn.utils import shuffle
+from cv2 import imread, cvtColor, resize, filter2D, medianBlur
+from cv2 import COLOR_BGR2GRAY
+from os import listdir
+import numpy as np
+# import matplotlib.pyplot as plt
+
+# Sizes 125, 150
+newSize = 150
+
+# Training data preperations
+train_loc = 'aug_testing/augments_dir/Merged/'
+
+xtrain = []
+ytrain = []
+
+for image_name in listdir(train_loc):
+    img = imread(train_loc + image_name)
+    img = cvtColor(img, COLOR_BGR2GRAY)
+
+    kernel = np.array([[-1/9,-1/9,-1/9], [-1/9,1,-1/9], [-1/9,-1/9,-1/9]])
+    img = filter2D(img, -1, kernel)
+    img = medianBlur(img, 3)
+
+    img = img/255.
+    img = np.expand_dims(img, 2)
+    xtrain.append(img)
+    ytrain.append(0 if 'norm' in image_name else 1)
+
+xtrain, ytrain = shuffle(xtrain, ytrain)
+xtrain, ytrain = np.asarray(xtrain), np.asarray(ytrain)
+
+# Validation data preperations
+validation_loc = 'val/Merged/'
+
+xval = []
+yval = []
+
+for image_name in listdir(validation_loc):
+    img = imread(validation_loc + image_name)
+    img = resize(cvtColor(img, COLOR_BGR2GRAY), (newSize, newSize))
+    
+    kernel = np.array([[-1/9,-1/9,-1/9], [-1/9,1,-1/9], [-1/9,-1/9,-1/9]])
+    img = filter2D(img, -1, kernel)
+    img = medianBlur(img, 3)
+    
+    img = img/255.
+    img = np.expand_dims(img, 2)
+    xval.append(img)
+    yval.append(0 if 'IM' in image_name else 1)
+
+xval, yval = shuffle(xval, yval)
+xval, yval = np.asarray(xval), np.asarray(yval)
+
+
+# Testing data preperations
+test_loc = 'test_merged/'
+
+
+xtest = []
+ytest = []
+   
+    
+for image_name in listdir(test_loc):
+    img = imread(test_loc + image_name)
+    img = resize(cvtColor(img, COLOR_BGR2GRAY), (newSize, newSize))
+    
+    kernel = np.array([[-1/9,-1/9,-1/9], [-1/9,1,-1/9], [-1/9,-1/9,-1/9]])
+    img = filter2D(img, -1, kernel)
+    img = medianBlur(img, 3)
+    
+    img = img / 255.
+    img = np.expand_dims(img, 2)
+    xtest.append(img)
+    ytest.append(0 if 'IM' in image_name else 1)
+
+xtest, ytest = shuffle(xtest, ytest)
+xtest, ytest = np.asarray(xtest), np.asarray(ytest)
+
+
+######################################### data preperation end #########################################
+# plt.imshow(np.squeeze(img), cmap='gray')
+######################################### model creation start #########################################
+
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import Dropout, Flatten, Dense
+from keras.callbacks import EarlyStopping
+# from keras import regularizers
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+es = EarlyStopping(monitor='val_acc', restore_best_weights=True, patience=5)
+
+model = Sequential()
+
+# Fs: 64, 128
+model.add(Conv2D(
+        128,
+        (3, 3),
+        activation='relu',
+        input_shape=(150, 150, 1)
+    ))
+
+model.add(MaxPooling2D())
+
+model.add(BatchNormalization())
+
+model.add(Conv2D(
+        64,
+        (3, 3),
+        activation='relu'
+    ))
+
+model.add(MaxPooling2D())
+
+model.add(BatchNormalization())
+
+model.add(Conv2D(
+        32,
+        (3, 3),
+        activation='relu'
+    ))
+
+model.add(MaxPooling2D())
+
+model.add(BatchNormalization())
+
+model.add(Flatten())
+
+model.add(Dense(
+        256,
+        activation='relu',
+    ))
+
+# Drops 0.3, 0.2/ 0.2,0.1
+model.add(Dropout(rate=0.2))
+
+model.add(Dense(
+        128,
+        activation='relu',
+    ))
+
+model.add(Dropout(rate=0.1))
+
+model.add(Dense(
+        1,
+        activation='sigmoid'
+    ))
+
+model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['acc']
+    )
+
+# BS: 32, 120, 64
+model.fit(
+        xtrain,
+        ytrain,
+        batch_size=64,
+        epochs=15,
+        validation_split=0.05,
+        callbacks=[es]
+        ,validation_data=(xval, yval)
+    )
+
+
+predictions = model.predict(xtest)
+
+predictions = 1*(predictions >= .65)
+
+cm = confusion_matrix(ytest, predictions)
+
+acc = accuracy_score(ytest, predictions)
+
+
+# model.evaluate(xtrain, ytrain)
+
+# model.evaluate(xval, yval)
+
+# model.evaluate(xtest, ytest)
+
